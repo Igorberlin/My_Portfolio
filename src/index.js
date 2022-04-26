a89fe26c57cc2f07bd4a9c39d4179ba6c0181caa
Igor <igorkuznecov@gmx.de>
2022-04-26 13:57:55 +0200

    add style

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
@@ -8,12 +7,10 @@ import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-    <StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
-    </StrictMode>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     rootElement
 );
