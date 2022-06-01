8be802adef3874d996662bed52441506eea0812b
Igor <igorkuznecov@gmx.de>
2022-06-01 15:46:06 +0200

    add typescript  project

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -8,6 +8,14 @@ const projects = [
     },
     {
         id: 2,
+        title: "Todo and persons list",
+        link: "https://igorberlin.github.io/React_Typescript_Project/",
+        description:
+            "React, Typescript,  React router dom, Materialize css and Styled components,",
+        image: "https://i.imgur.com/AYfhmO1.jpg",
+    },
+    {
+        id: 3,
         title: "Books Library",
         link: "https://Igorberlin.github.io/Books_Project",
         description:
@@ -15,7 +23,7 @@ const projects = [
         image: "https://i.imgur.com/WP4Ksz6.jpg",
     },
     {
-        id: 3,
+        id: 4,
         title: "Food Category",
         link: "https://Igorberlin.github.io/Food_Category_Project/",
         description:
@@ -23,7 +31,7 @@ const projects = [
         image: "https://i.imgur.com/tf7Gb9r.jpg",
     },
     {
-        id: 4,
+        id: 5,
         title: "Golden One Project Bootstrap",
         link: "https://igorberlin.github.io/Golden_One_Project_Bootstrap/",
         description:
@@ -31,7 +39,7 @@ const projects = [
         image: "https://i.imgur.com/va8JOXe.jpg",
     },
     {
-        id: 5,
+        id: 6,
         title: "Photo Gallery",
         link: "https://Igorberlin.github.io/Photo_Gallery_Project/",
         description:
@@ -39,7 +47,7 @@ const projects = [
         image: "https://i.imgur.com/x8q18gZ.jpg",
     },
     {
-        id: 6,
+        id: 7,
         title: "Recipes",
         link: "https://igorberlin.github.io/Recipes_Project/",
         description:
@@ -47,7 +55,7 @@ const projects = [
         image: "https://i.imgur.com/DJkwK7k.jpg",
     },
     {
-        id: 7,
+        id: 8,
         title: "TinyOne Page",
         link: "https://igorberlin.github.io/TinyOne_Project/",
         description:
@@ -55,42 +63,42 @@ const projects = [
         image: "https://i.imgur.com/kY3Mb94.jpg",
     },
     {
-        id: 8,
+        id: 9,
         title: "Landing Page Training",
         link: "https://igorberlin.github.io/Landing_Page_Training/",
         description: "HTML and CSS Layout, The position Property, z-index",
         image: "https://i.imgur.com/q2JmanL.jpg",
     },
     {
-        id: 9,
+        id: 10,
         title: "Landing Page Bootstrap",
         link: "https://igorberlin.github.io/Easy_Landing_Page_Bootstrap/",
         description: "Bootstrap",
         image: "https://i.imgur.com/jqyIIaW.jpg",
     },
     {
-        id: 10,
+        id: 11,
         title: "Easy Landing Page",
         link: "https://igorberlin.github.io/Easy_Landing_Page_HTML_CSS/",
         description: "HTML, CSS",
         image: "https://i.imgur.com/WGJPR9l.jpg",
     },
     {
-        id: 11,
+        id: 12,
         title: "Burger Menu Component",
         link: "https://igorberlin.github.io/burger_menu/",
         description: "React, HTML, CSS",
         image: "https://i.imgur.com/0SJeZHI.jpg",
     },
     {
-        id: 12,
+        id: 13,
         title: "Phone Book",
         link: "https://igorberlin.github.io/phone_book_project/",
         description: "HTML, CSS, JS",
         image: "https://i.imgur.com/xJT0O3r.jpg",
     },
     {
-        id: 13,
+        id: 14,
         title: "Before and After Slider",
         link: "https://igorberlin.github.io/Before_After/",
         description: "HTML, CSS, JS",
